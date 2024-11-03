8ec6e69f328b95fe2d23a68bad294115c934782c
Gaurav Prasanna <gpece2001@gmail.com>
2024-11-03 00:23:46 +0000

    Initial commit

---
+++ linear_reg_grad_desc.py
@@ -0,0 +1,29 @@
+'''
+Linear Regression Using Gradient Descent (easy)
+Write a Python function that performs linear regression using gradient descent.
+The function should take NumPy arrays X (features with a column of ones for the intercept)
+and y (target) as input, along with learning rate alpha and the number of iterations,
+and return the coefficients of the linear regression model as a NumPy array.
+Round your answer to four decimal places. -0.0 is a valid result for rounding a
+very small number.
+'''
+
+import numpy as np
+
+def linear_regression_gradient_descent(X: np.ndarray, y: np.ndarray, alpha: float, iterations: int) -> np.ndarray:
+    # Your code here, make sure to round
+    m, n = X.shape
+    theta = np.zeros((n, 1))
+    y_reshaped = y.reshape(-1, 1)
+    X_trans = np.transpose(X)
+
+    for _ in range(iterations):
+        predictions = np.matmul(X, theta)
+        error = predictions - y_reshaped
+        updates = np.matmul(X_trans, error)
+        updated_m = updates/m
+        theta -= alpha * updated_m
+    return np.round(theta.flatten(), 4)
+
+print(linear_regression_gradient_descent(np.array([[1, 1], [1, 2], [1, 3]]), np.array([1, 2, 3]), 0.01, 1000))
+## [0.1107, 0.9513]
---
+++ linear_reg_normal_eqn.py
@@ -0,0 +1,28 @@
+'''
+Linear Regression Using Normal Equation (easy)
+Write a Python function that performs linear regression using the normal equation.
+The function should take a matrix X (features) and a vector y (target) as input,
+and return the coefficients of the linear regression model. Round your answer to four decimal
+places, -0.0 is a valid result for rounding a very small number.
+Example:
+        input: X = [[1, 1], [1, 2], [1, 3]], y = [1, 2, 3]
+        output: [0.0, 1.0]
+        reasoning: The linear model is y = 0.0 + 1.0*x, perfectly fitting the input data.
+'''
+
+import numpy as np
+
+def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> list[float]:
+    X = np.array(X)
+    y = np.array(y).reshape(-1)
+    X_trans = np.transpose(X)
+    X_prod = np.linalg.inv(np.matmul(X_trans, X))
+    y_prod = np.matmul(X_trans, y)
+    theta = np.matmul(X_prod, y_prod)
+    theta = np.round(theta, 4).flatten().tolist()
+    return theta
+
+print(linear_regression_normal_equation([[1, 1], [1, 2], [1, 3]], [1, 2, 3]))
+## [-0.0, 1.0]
+print(linear_regression_normal_equation([[1, 3, 4], [1, 2, 5], [1, 3, 2]], [1, 2, 1]))
+## [4.0, -1.0, -0.0]
